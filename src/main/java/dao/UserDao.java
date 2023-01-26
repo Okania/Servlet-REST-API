0030a7266439077da610ba400bf4bd16c8444888
Okania <evans.okania@strathmore.edu>
2023-01-26 19:59:04 +0300

    files updates

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -8,14 +8,14 @@ public class UserDao {
 	
 	
     
-    public HashMap<String,User> populateUsers(){
+    public HashMap<Integer,User> populateUsers(){
     	
-    	HashMap<String, User> userMap = new HashMap<>();
+    	HashMap<Integer, User> userMap = new HashMap<>();
     	
-    	
-    	
-    	
-
+    	userMap.put(1, new User(1, "Evans", "Odhis","ICS", 21));
+    	userMap.put(2, new User(2, "John", "Doe","BCOM", 20));
+    	userMap.put(3, new User(1, "Naomi", "Okania","BBIT", 24));
+    	userMap.put(4, new User(1, "Phil", "Foden","CNS", 23));	
     	
     }
 
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -15,45 +15,25 @@ public class User {
 		this.course = course;
 		this.age = age;
 	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
+		
 	public int getId() {
 		return id;
 	}
 	
 	
-	public void setFname(String fname) {
-		this.first_name = fname;
-	}	
 	public String getFname() {
 		return first_name;
 	}
 	
-	
-	public void setLname(String lname) {
-		this.last_name = lname;
-	}
-	
+		
 	public String getLname() {
 		return last_name;
-	}
-	
-	
-	public void setCourse(String course) {
-		this.course = course;
 	}
 	
 	public String getCourse() {
 		return course;
 	}
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
+		
 	public int getAge() {
 		return age;
 	}
