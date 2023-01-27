5e378138e8c0f7671ec3dce9218b0b01fc62cef9
Okania <evans.okania@strathmore.edu>
2023-01-27 09:28:58 +0300

    update on user DAO file

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -4,18 +4,16 @@ import java.util.HashMap;
 
 import model.User;
 
-public class UserDao {
-	
-	
+public class UserDao {	
     
-    public HashMap<Integer,User> populateUsers(){
+    private UserDao(){
     	
     	HashMap<Integer, User> userMap = new HashMap<>();
     	
     	userMap.put(1, new User(1, "Evans", "Odhis","ICS", 21));
     	userMap.put(2, new User(2, "John", "Doe","BCOM", 20));
-    	userMap.put(3, new User(1, "Naomi", "Okania","BBIT", 24));
-    	userMap.put(4, new User(1, "Phil", "Foden","CNS", 23));	
+    	userMap.put(3, new User(3, "Naomi", "Okania","BBIT", 24));
+    	userMap.put(4, new User(4, "Phil", "Foden","CNS", 23));	
     	
     }
 
