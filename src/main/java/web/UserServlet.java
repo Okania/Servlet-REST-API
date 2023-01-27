94a1f99326579e4d9fc9ef103f5b78518f0ef11b
Okania <evans.okania@strathmore.edu>
2023-01-27 12:47:54 +0300

    modified user servlet

--- src/main/java/web/UserServlet.java
+++ src/main/java/web/UserServlet.java
@@ -1,5 +1,6 @@
 package web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
@@ -64,9 +65,37 @@ public class UserServlet extends HttpServlet {
 			return;			
 		}
         
-        sendAsJson(res, UserDao.getUsers().get(userId));
-        
+        sendAsJson(res, UserDao.getUsers().get(userId));        
         
 	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException{
+		
+		String pathInfo = req.getPathInfo();
+		
+		if (pathInfo == null || pathInfo == "/") {
+			StringBuilder buffer = new StringBuilder();
+			BufferedReader reader = req.getReader();
+			String line;
+			
+			while((line = reader.readLine()) != null) {
+				buffer.append(line);				
+			}
+			
+			String payload = buffer.toString();
+			User user = gson.fromJson(payload, User.class);
+			UserDao.getUsers().put(user.getId(), user);
+			
+			sendAsJson(res, user);		
+		}
+		else {
+			res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+	}
+	
+	
 
 }
